bf734c7a33d623d63d02d23ec37e9aa65243406b
JyunKim <kann1997@naver.com>
2020-11-25 18:47:32 +0900

    Add room filters

--- myhome/api/views.py
+++ myhome/api/views.py
@@ -1,10 +1,28 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django_filters.rest_framework import DjangoFilterBackend, filters, FilterSet
+from django.shortcuts import get_object_or_404
 from .models import Room, Tenant, Review, Photo, Contract
 from .serializers import RoomSerializer, TenantSerializer, ReviewSerializer, PhotoSerializer, ContractSerializer
+
+
+class RoomFilter(FilterSet):
+    room_type = filters.MultipleChoiceFilter(choices=Room.ROOM_TYPE_CHOICES)
+    deposit = filters.RangeFilter()
+    monthly_rent = filters.RangeFilter()
+    management_fee = filters.RangeFilter()
+    space__min = filters.NumberFilter(field_name='space', method='filter_space_gte')
+    space__max = filters.NumberFilter(field_name='space', method='filter_space_lte')
+    
+    def filter_space_gte(self, queryset, name, value):
+        return queryset.filter(space__gte=int(value)*3.305785)
+
+    def filter_space_lte(self, queryset, name, value):
+        return queryset.filter(space__lte=int(value)*3.305785)
 
 
 class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+    filterset_class = RoomFilter

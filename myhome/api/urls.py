477c380c290d207746c4e74f06f3067ba1187cd6
JyunKim <kann1997@naver.com>
2021-01-04 02:19:49 +0900

    Add jwt urls

--- myhome/api/urls.py
+++ myhome/api/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 from . import views
 
 
@@ -11,4 +16,10 @@ router.register(r'comments', views.CommentViewSet)
 router.register(r'users', views.UserViewSet)
 router.register(r'mentors', views.MentorViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # email, password
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # refresh: refresh token
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),  # token: token
+    path('login/', views.login, name='login'),
+    path('', include(router.urls)),
+]

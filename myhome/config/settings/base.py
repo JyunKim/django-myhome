4d9ed5c5d9f50880c925fb3cdb4ef60043330d98
JyunKim <kann1997@naver.com>
2021-01-10 23:12:50 +0900

    Modify nginx conf applying certbot https redirection

--- myhome/config/settings/base.py
+++ myhome/config/settings/base.py
@@ -149,3 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')

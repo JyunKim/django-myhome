280f90daf023a74579abf2af12a28c3b21e6ad2d
JyunKim <kann1997@naver.com>
2021-01-15 03:22:23 +0900

    Move S3 settings to prod.py

--- myhome/config/settings/base.py
+++ myhome/config/settings/base.py
@@ -148,19 +148,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_HTTPONLY = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-
-DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
-STATICFILES_STORAGE = 'config.storages.StaticStorage'
-
-AWS_ACCESS_KEY_ID = secrets["ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = secrets["SECRET_ACCESS_KEY"]
-AWS_STORAGE_BUCKET_NAME = secrets["BUCKET_NAME"]
-AWS_S3_REGION_NAME = secrets["REGION_NAME"]
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}

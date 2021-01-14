43ea098a9768107fedd6a6ab285cbdd46d47accf
JyunKim <kann1997@naver.com>
2021-01-15 00:24:36 +0900

    Modify S3 settings

--- myhome/config/storages.py
+++ myhome/config/storages.py
@@ -3,8 +3,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    default_acl = 'private'
+    location = 'media'
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    default_acl = 'private'
+    location = 'static'
